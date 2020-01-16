e1a75abd61607716ad67c4bc54db64be1cd2c686
Matt Bessey <matt.bessey@immersivelabs.co.uk>
2020-01-16 20:37:04 +0000

    add gemfile + erb support

--- README.md
+++ README.md
@@ -22,10 +22,10 @@ Uses the standard VS Code Formatting Extension API.
 
 If you want to format a file one off, you will find `> Format Document` in your command palette.
 
-Out of the box, Rufo will format your documents on Save. To change this you may override `editor.formatOnSave` either, or more specifically just for the Ruby language:
+Out of the box, Rufo will format your Ruby, ERB, and Gemfile documents on Save. To change this you may override `editor.formatOnSave` either, or more specifically just for the Ruby language:
 
 ```js
-  "[ruby]": {
+  "[ruby]": { // or [erb] / [gemfile]
     "editor.formatOnSave": true
   }
 ```
--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,16 @@
 import { languages } from "vscode";
 import { RubyDocumentFormatter } from "./rufo";
 
+const languagesSupported = ["ruby", "erb", "gemfile"];
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate() {
-  languages.registerDocumentFormattingEditProvider(
-    { language: "ruby", scheme: "file" },
-    new RubyDocumentFormatter()
+  languagesSupported.map(language =>
+    languages.registerDocumentFormattingEditProvider(
+      { language, scheme: "file" },
+      new RubyDocumentFormatter()
+    )
   );
 }
 
