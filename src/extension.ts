476df7334d9e1de55ddb1603cdcede8934a8ca4c
Max Brosnahan <maximilianbrosnahan@gmail.com>
2020-01-19 19:49:13 +0100

    Update extension setup
    Why: This fixes the debug task which allows the extension to be more easily run and debugged.

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
-"use strict";
-import { languages } from "vscode";
+import * as vscode from "vscode";
 import { RubyDocumentFormatter } from "./rufo";
 
 const languagesSupported = ["ruby", "erb", "gemfile"];
@@ -7,13 +6,11 @@ const languagesSupported = ["ruby", "erb", "gemfile"];
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate() {
-  languages.registerDocumentFormattingEditProvider(
+  vscode.languages.registerDocumentFormattingEditProvider(
     languagesSupported.map(language => ({ language, scheme: "file" })),
     new RubyDocumentFormatter()
   );
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-  // No-op
-}
+export function deactivate() {}
