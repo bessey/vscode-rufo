ac8c2ffa0101d4ed7014f52316d0e3d3932f697f
Max Brosnahan <maximilianbrosnahan@gmail.com>
2020-01-21 07:33:57 +0100

    Address PR feedback

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-import * as vscode from "vscode";
+import { languages } from "vscode";
 import { RubyDocumentFormatter } from "./rufo";
 
 const languagesSupported = ["ruby", "erb", "gemfile"];
@@ -6,7 +6,7 @@ const languagesSupported = ["ruby", "erb", "gemfile"];
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate() {
-  vscode.languages.registerDocumentFormattingEditProvider(
+  languages.registerDocumentFormattingEditProvider(
     languagesSupported.map(language => ({ language, scheme: "file" })),
     new RubyDocumentFormatter()
   );
